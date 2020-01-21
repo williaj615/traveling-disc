5c55782ecb636b750524b5d95bc406d72ae2e166
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-20 20:11:18 -0600

    can login using google auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import firebase from 'firebase/app';
+
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
-    </div>
-  );
+firebaseConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+        {/* if they are authenticated, load the team roster */}
+        {/* else show login button */}
+        {
+          (authed) ? (<div>You logged in</div>) : (<Auth />)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
