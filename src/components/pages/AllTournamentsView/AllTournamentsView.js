236e492ff257d13b5c4aad2de2bde928c4a92576
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-21 19:08:22 -0600

    created routes for different pages and views, they work correctly

---
+++ src/components/pages/AllTournamentsView/AllTournamentsView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AllTournamentsView extends React.Component {
+  render() {
+    return (
+      <div>Viewing All Tournaments!</div>
+    );
+  }
+}
+
+export default AllTournamentsView;
---
+++ src/components/pages/MyTournamentsView/MyTournamentsView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyTournamentsView extends React.Component {
+  render() {
+    return (
+      <div>Viewing Your Personal Tournaments!</div>
+    );
+  }
+}
+
+export default MyTournamentsView;
