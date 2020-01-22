870469b93230728efb165512d8074bda1edfb675
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-21 20:57:07 -0600

    WIP

--- src/components/pages/AllTournamentsView/AllTournamentsView.js
+++ src/components/pages/AllTournamentsView/AllTournamentsView.js
@@ -16,14 +16,49 @@ class AllTournamentsView extends React.Component {
       .catch((err) => console.error('error on get all tournaments', err));
   }
 
+  grassFilter = (e) => {
+
+  }
+
+  beachFilter = () => {
+
+  }
+
+  usFilter = () => {
+
+  }
+
+  internationalFilter = () => {
+
+  }
+
   componentDidMount() {
     this.getTournaments();
   }
 
   render() {
     return (
-      <div className="all-tournaments-container d-flex flex-row flex-wrap justify-content-around">
-        {this.state.tournaments.map((tournament) => (<Tournament key={tournament.id} tournament={tournament} />))}
+      <div className="page-container">
+        <h1>All Tournaments</h1>
+        <div className="d-flex flex-row">
+          <div className="all-tournaments-container d-flex flex-row flex-wrap justify-content-around col-10">
+            {this.state.tournaments.map((tournament) => (<Tournament key={tournament.id} tournament={tournament} />))}
+          </div>
+          <div className="filter-form col-2">
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" onChange={this.grassFilter}></input>
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Show Only Grass
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></input>
+              <label className="form-check-label" htmlFor="exampleRadios2">
+                Show Only Beach
+              </label>
+            </div>
+            </div>
+        </div>
       </div>
     );
   }
