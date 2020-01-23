1aa2d96221e92a099fa5f989df2eb5548ea56c71
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-23 17:50:49 -0600

    filters on tournament read work correctly

--- src/components/pages/AllTournamentsView/AllTournamentsView.js
+++ src/components/pages/AllTournamentsView/AllTournamentsView.js
@@ -5,31 +5,39 @@ import Tournament from '../../shared/Tournament/Tournament';
 class AllTournamentsView extends React.Component {
   state = {
     tournaments: [],
-
+    filteredTournaments: [],
   }
 
   getTournaments = () => {
     tournamentData.getAllTournaments()
       .then((tournaments) => {
-        this.setState({ tournaments });
+        this.setState({ tournaments, filteredTournaments: tournaments });
       })
       .catch((err) => console.error('error on get all tournaments', err));
   }
 
   grassFilter = (e) => {
-
+    e.preventDefault();
+    const grassTournaments = this.state.tournaments.filter((x) => !x.isBeach);
+    this.setState({ filteredTournaments: grassTournaments });
   }
 
-  beachFilter = () => {
-
+  beachFilter = (e) => {
+    e.preventDefault();
+    const beachTournaments = this.state.tournaments.filter((x) => x.isBeach);
+    this.setState({ filteredTournaments: beachTournaments });
   }
 
-  usFilter = () => {
-
+  usFilter = (e) => {
+    e.preventDefault();
+    const usTournaments = this.state.tournaments.filter((x) => !x.isInternational);
+    this.setState({ filteredTournaments: usTournaments });
   }
 
-  internationalFilter = () => {
-
+  internationalFilter = (e) => {
+    e.preventDefault();
+    const internationalTournaments = this.state.tournaments.filter((x) => x.isInternational);
+    this.setState({ filteredTournaments: internationalTournaments });
   }
 
   componentDidMount() {
@@ -42,31 +50,37 @@ class AllTournamentsView extends React.Component {
         <h1>All Tournaments</h1>
         <div className="d-flex flex-row">
           <div className="all-tournaments-container d-flex flex-row flex-wrap justify-content-around col-10">
-            {this.state.tournaments.map((tournament) => (<Tournament key={tournament.id} tournament={tournament} />))}
+            {this.state.filteredTournaments.map((tournament) => (<Tournament key={tournament.id} tournament={tournament} />))}
           </div>
           <div className="filter-form col-2">
             <div className="form-check">
-              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" onChange={this.grassFilter}></input>
-              <label className="form-check-label" htmlFor="exampleRadios1">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="grassRadios" value="option1" onChange={this.grassFilter}></input>
+              <label className="form-check-label" htmlFor="grassRadios">
                 Show Only Grass
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" onChange={this.beachFilter}></input>
-              <label className="form-check-label" htmlFor="exampleRadios2">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="beachRadios" value="option2" onChange={this.beachFilter}></input>
+              <label className="form-check-label" htmlFor="beachRadios">
                 Show Only Beach
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option2" onChange={this.usFilter}></input>
-              <label className="form-check-label" htmlFor="exampleRadios1">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="usRadios" value="option3" onChange={this.usFilter}></input>
+              <label className="form-check-label" htmlFor="usRadios">
                 Show Only U.S
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" onChange={this.internationalFilter}></input>
-              <label className="form-check-label" htmlFor="exampleRadios2">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="intlRadios" value="option4" onChange={this.internationalFilter}></input>
+              <label className="form-check-label" htmlFor="intlRadios">
                 Show Only International
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="exampleRadios" id="clearRadios" value="option5" onChange={this.getTournaments}></input>
+              <label className="form-check-label" htmlFor="clearRadios">
+                Clear Filters
               </label>
             </div>
           </div>
